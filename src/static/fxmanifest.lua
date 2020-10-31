1b12e06d77a0b999b2f90ef4ddbcb0b633c8be79
jaimeadf <jaimeadanielfilho@hotmail.com>
2020-10-31 17:25:04 -0300

    Update resource and fx version

--- src/static/fxmanifest.lua
+++ src/static/fxmanifest.lua
@@ -1,9 +1,9 @@
-fx_version 'adamant'
+fx_version 'bodacious'
 game 'common'
 
 author 'Jaime Filho <https://github.com/jaimeadf>'
 description "A resource for FiveM and RedM that takes a screenshot of a player and uploads it to a discord's webhook."
-version '1.2'
+version '1.2.0'
 url 'https://github.com/jaimeadf/discord-screenshot'
 
 server_script 'dist/server.js'
